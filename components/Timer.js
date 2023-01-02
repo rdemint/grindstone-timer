72a24719c2f167e5ef89562f773744688d3c18c2
Raines Demint <raines@qaladder.com>
2023-01-01 20:31:38 -0800

    updating timer UI

--- components/Timer.js
+++ components/Timer.js
@@ -1,49 +1,38 @@
-import React, {useState, useEffect} from "react"
+import React, {useState, useRef, useEffect} from "react"
 
 export default function Timer() {
-    const [isActive, setIsActive] = useState(false)
-    const [isPaused, setIsPaused] = useState(true)
-    const [time, setTime] = useState(0)
+    
+    const startTime = Date.parse(new Date())
+    const [time, setTime] = useState(10)
 
-    const handleStart = ()=> {
-        setIsActive(true)
-        setIsPaused(false)
-    }
+    const [isActive, setActive] = useState(false)
+    const [isPaused, setisPaused] = useState(false)
 
     const handleReset = () => {
-        setIsActive(false)
-        setTime(0)
+        setActive(false)
+        setisPaused(false)
+        setTime(10)
     }
-
 
     useEffect(()=> {
         let interval = null
-        if (isActive && isPaused === false) {
-            interval = setInterval(
-                ()=> {
-                    setTime((time) => time + 10)
-                }, 10
-            )
-        } else {
-            clearInterval(interval)
+        if (isActive && isPaused == false) {
+            interval = setInterval(()=> setTime(time-1), 1000)
         }
-
         return () => clearInterval(interval)
-        }, [isActive, isPaused]
-    )
+    })
+
 
     return (
         <div className="flex flex-col justify-center items-center w-full">
-                <div className="flex flex-col bg-slate-500 h-24 border border-slate-600 rounded max-w-5xl border-white">
-                    <div className="h-24 bg-slate-700 text-center">
-                        {("0" + Math.floor((time / 60000) % 60)).slice(-2)}:
-                        {("0" + Math.floor((time / 1000) % 60)).slice(-2)}.
-                        {("0" + Math.floor((time / 10) % 100)).slice(-2)}
 
+                <div className="flex flex-col bg-slate-700 h-24 border border-slate-600 rounded max-w-5xl border-white">
+                    <div className="h-24 bg-slate-700 justify-center text-center flex space-x-4">
+                        <div>{time}</div>
                     </div>
                     <div className="flex items-center justify-center space-x-4">
-                        <button onClick={()=> handleStart()} id="startTimer" className="bg-sky-400  rounded p-2">Start</button>
-                        <button onClick={()=> setIsPaused(true)} id="pauseTimer" className="bg-sky-600  rounded p-2">Pause</button>
+                        <button onClick={()=> setActive(true)} id="startTimer" className="bg-sky-400  rounded p-2">Start</button>
+                        <button onClick={()=> setisPaused(true)} id="pauseTimer" className="bg-sky-600  rounded p-2">Pause</button>
                         <button onClick={()=> handleReset()} id="resetTimer" className="bg-pink-400 rounded p-2">Reset</button>
                     </div>
                 </div>
