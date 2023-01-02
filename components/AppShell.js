56bccc22a743035e7aba7c7aab786d3cbc535263
Raines Demint <raines@qaladder.com>
2023-01-01 16:27:56 -0800

    bootstrap new next project with tailwind

---
+++ components/AppShell.js
@@ -0,0 +1,12 @@
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+export default function AppShell({children}) {
+    return (
+        <div className="min-h-screen flex flex-col justify-between bg-slate-900 text-slate-300">
+            <Header />
+            <main>{children}</main>
+            <Footer />
+        </div>
+    )
+}
