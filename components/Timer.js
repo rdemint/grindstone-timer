4abe89539246e5d32e988c58b7ca1d97407e3000
Raines Demint <raines@qaladder.com>
2023-01-03 20:56:25 -0800

    Added preparation countdown before workout

--- components/Timer.js
+++ components/Timer.js
@@ -1,34 +1,32 @@
 import React, {useState, useReducer, useEffect} from "react"
 
-
-
-
 export default function Timer() {
-
-
+    
     let [workoutConfig, setWorkoutConfig] = useState({
+        prepInterval: 3,
         workInterval: 10,
         restInterval: 90,
         numIntervals: 10
     })
 
-    const timerStatuses = {reset: 'reset', start: 'start', pause: 'pause', complete: 'complete'}
-    const intervalTypes = {restInterval: 'restInterval', workInterval: 'workInterval'}
+    const timerStatuses = {notStarted: 'notstarted', started: 'started', paused: 'paused', completed: 'completed'}
+    const intervalTypes = {restInterval: 'REST', workInterval: 'WORK', prepInterval: 'PREP'}
 
-    const [timer, setTimer] = useState({
-        status: timerStatuses.reset,
+    const initTimer = {
+        status: timerStatuses.notStarted,
         currentInterval: 1,
-        currentIntervalType: intervalTypes.restInterval,
-
-    })
+        currentIntervalType: intervalTypes.prepInterval,
+    }
     
-    let [time, setTime] = useState(workoutConfig.workInterval)
+    const [timer, setTimer] = useState(initTimer)
+    
+    let [time, setTime] = useState(workoutConfig.prepInterval)
     
 
     const handleStartTimer = () => {
         setTimer({
             ...timer,
-            status: timerStatuses.start,
+            status: timerStatuses.started,
         })
 
     }
@@ -36,25 +34,29 @@ export default function Timer() {
     const handlePauseTimer = () => {
         setTimer({
             ...timer,
-            status: timerStatuses.pause
+            status: timerStatuses.paused
         })
     }
 
     const handleResetTimer = () => {
-        setTimer({
-            ...timer,
-            status: timerStatuses.reset,
-            currentInterval: 1,
-            currentIntervalType: intervalTypes.workInterval,
-        })
-        
-        setTime(workoutConfig.workInterval)
+        setTimer(initTimer)
+        setTime(workoutConfig.prepInterval)
     }
 
     
     const completeInterval = () => {
+
+        if(timer.currentIntervalType == intervalTypes.prepInterval) {
+            //work interval always follows prep
+            setTimer({
+                ...timer,
+                currentIntervalType: intervalTypes.workInterval,
+            })
+            setTime(workoutConfig.workInterval)
+        }
         
         if(timer.currentIntervalType == intervalTypes.workInterval) {
+            //rest interval always follows work interval
             setTimer({
                 ...timer,
                 currentIntervalType: intervalTypes.restInterval
@@ -62,6 +64,7 @@ export default function Timer() {
             setTime(workoutConfig.restInterval)
         } 
         else if(timer.currentIntervalType === intervalTypes.restInterval) {
+            //increment interval once rest is completed
             if(timer.currentInterval < workoutConfig.numIntervals) {
                 setTimer(
                     {
@@ -72,20 +75,22 @@ export default function Timer() {
                 setTime(workoutConfig.workInterval)
             }    
             else if(timer.currentInterval === workoutConfig.numIntervals) {
+                //all intervals are complete
                 setTimer({
                     ...timer,
-                    status: timerStatuses.complete
+                    status: timerStatuses.completed
                 })
             }
         }        
     }
 
     const getTimerTheme = () => {
-        if(timer.status === timerStatuses.start && timer.currentIntervalType === intervalTypes.workInterval) {
-            return 'bg-green-700'
-        }
-        else if(timer.status === timerStatuses.complete) {
+        if(timer.status === timerStatuses.completed) {
             return 'bg-yellow-500'
+        }
+
+        else if(timer.status === timerStatuses.started && timer.currentIntervalType === intervalTypes.workInterval) {
+            return 'bg-green-700'
         }
         else {
             return 'bg-slate-700'
@@ -96,7 +101,7 @@ export default function Timer() {
     
     useEffect(()=> {
         let interval = null
-        if (timer.status === timerStatuses.start && time > 0) {
+        if (timer.status === timerStatuses.started && time > 0) {
             interval = setInterval(()=> setTime(time-1), 1000)
         }
         else if (time <= 0) {
@@ -113,11 +118,10 @@ export default function Timer() {
                         <div>INTERVAL</div>
                         <div>{timer.currentInterval} / {workoutConfig.numIntervals}</div>
                     </div>
-                    <div className="justify-center items-center text-center flex space-x-4 text-6xl h-24">
-                        {
-                            timer.currentIntervalType === intervalTypes.restInterval ? <div className="text-slate-300">REST:</div> : <div>WORK:</div>
-                        }
-                        <div className="text-6xl">{time}</div>
+                    <div className="flex flex-col justify-center items-center text-center space-y-4 text-6xl h-24">
+                        <div>{timer.currentIntervalType}</div>
+                        <div>{time}</div>
+                        
                     </div>
                     <div className="flex items-center justify-center space-x-4 text-slate-100">
                         <button onClick={()=> handleStartTimer()} id="startTimer"  className="bg-green-500  rounded p-2 w-16">START</button>
@@ -126,18 +130,15 @@ export default function Timer() {
                     </div>
                 </div>
                 <div className="flex flex-col space-y-4 bg-slate-700 rounded max-w-5xl p-8">
-                    <form className="flex justify-center space-x-4">
+                    <form className="flex-col space-y-4 md:flex-row md:space-x-4 justify-center">
+                        <label className="w-12"l>Prep</label> 
+                        <input className="w-8 bg-slate-500 rounded px-1 text-center" type="text" value={workoutConfig.prepInterval} onChange={(e)=> setWorkoutConfig({...workoutConfig, prepInterval: e.target.value})}/>
                         <label className="w-12">Work</label>
                         <input className="w-8 bg-slate-500 rounded px-1 text-center" type="text" value={workoutConfig.workInterval} onChange={(e)=> {
                             setWorkoutConfig({...workoutConfig, workInterval: e.target.value})
-                            setTime(e.target.value)
                             }}/>
-                    </form> 
-                    <form className="flex justify-center space-x-4"> 
                         <label className="w-12"l>Rest</label> 
                         <input className="w-8 bg-slate-500 rounded px-1 text-center" type="text" value={workoutConfig.restInterval} onChange={(e)=> setWorkoutConfig({...workoutConfig, restInterval: e.target.value})}/>
-                    </form> 
-                    <form className="flex justify-center space-x-4"> 
                         <label className="w-12"l>Intervals</label> 
                         <input className="w-8 bg-slate-500 rounded px-1 text-center" type="text" value={workoutConfig.numIntervals} onChange={(e)=> setWorkoutConfig({...workoutConfig, numIntervals: e.target.value})}/>
                     </form>
