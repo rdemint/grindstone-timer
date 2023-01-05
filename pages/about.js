ef38e0d81720b59ac1d2c289c685d1c329b7f401
Raines Demint <raines@qaladder.com>
2023-01-04 16:58:28 -0800

    Smaller blur loading image

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,7 @@ export default function About() {
     return (
                 <div className="flex justify-center max-w-3xl mx-auto">
                 <div class="prose">
-                    <Image src="/crown_jewel20_2mb.jpg" height={432} width={768} alt="A man rock climbing a large boulder." placeholder="blur" blurDataURL="/crown_jewel20_small_blur.jpg"/>
+                    <Image src="/crown_jewel20_2mb.jpg" height={432} width={768} alt="A man rock climbing a large boulder." placeholder="blur" blurDataURL="/crown_jewel20_small_blur2.jpg"/>
                     <div className='px-10 pt-10 flex flex-col space-y-8'>
                         <h2 className="text-slate-200 text-2xl">About me</h2>
                         <p>I have been climbing since 2012, but only in the past few years have I realized how important hangboard training is for overall climbing performance.</p>
