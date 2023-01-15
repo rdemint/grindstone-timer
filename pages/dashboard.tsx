ae588b65bdc0191fc47564858f311a43716aeb46
Raines Demint <raines@qaladder.com>
2023-01-15 07:06:19 -0800

    Findall intervals with prisma implemented

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,59 +1,71 @@
-import {PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
-export async function getStaticProps(context) {
+export async function getServerSideProps(context) {
 
     const prisma = new PrismaClient()
-    const intervals = [
-        {
-            leftHold: '30',
-            rightHold: '15',
-            restInterval: 30,
-            workInterval: 20
-        },
-        {
-            leftHold: '20',
-            rightHold: '10',
-            restInterval: 20,
-            workInterval: 10
-        },
-        {
-            leftHold: '10',
-            rightHold: '10',
-            restInterval: 90,
-            workInterval: 10
-        },
-    ]
+
+    let pIntervals = await prisma.interval.findMany()
+
+    
+    // const intervals = [
+    //     {
+    //         leftHold: '30',
+    //         rightHold: '15',
+    //         restInterval: 30,
+    //         workInterval: 20
+    //     },
+    //     {
+    //         leftHold: '20',
+    //         rightHold: '10',
+    //         restInterval: 20,
+    //         workInterval: 10
+    //     },
+    //     {
+    //         leftHold: '10',
+    //         rightHold: '10',
+    //         restInterval: 90,
+    //         workInterval: 10
+    //     },
+    // ]
     
     return {
-        props: {intervals}
+        props: {pIntervals}
     }
 }
 
-export default function DashBoard({ intervals }) {
+export default function DashBoard({ pIntervals }) {
     return (
-        <div className="flex flex-col items-center max-w-5xl space-y-2">
+        <div className="flex flex-col items-center space-y-2 w-full">
             <div>
-                <table className="table-fixed">
-                    <thead>
-                        <td>Left</td>
-                        <td>Right</td>
-                        <td>Rest</td>
-                        <td>Work</td>
-                    </thead>
-                    <tbody>
-                        {intervals.map(
-                            (interval, i) => (
-                                <tr key={i}>
-                                    <td>{interval.leftHold}</td>
-                                    <td>{interval.rightHold}</td>
-                                    <td>{interval.restInterval}</td>
-                                    <td>{interval.workInterval}</td>
-                                </tr>
-                            )
-                        )}
-                    </tbody>
-                </table>
-            </div>
+            {pIntervals.length > 0 ?
+                <>
+                    <h4 className="text-center py-4 text-3xl">Workout Summary</h4>
+                    <table className="w-72">
+                        <thead>
+                            <tr className="w-full">
+                                <th>Left</th>
+                                <th>Right</th>
+                                <th>Work</th>
+                                <th>Rest</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                                {pIntervals.map((interval, i) => (
+                                    <tr key={i} className="text-center">
+                                        <td>{interval.leftHold}</td>
+                                        <td>{interval.rightHold}</td>
+                                        <td>{interval.workInterval}</td>
+                                        <td>{interval.restInterval}</td>
+                                    </tr>
+                                ))}
+
+                        </tbody>
+                    </table>
+                </>
+                :
+                <div className="text-slate-400 py-4">No intervals completed yet.</div>
+            }
+        </div>
         </div>
     )
 }
