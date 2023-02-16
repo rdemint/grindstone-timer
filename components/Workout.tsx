da04329bae91ad20b4ee5da02d1294574ad44d53
Raines Demint <raines@qaladder.com>
2023-02-16 13:58:54 -0800

    Fixed key prop error, props in Workouts component

--- components/Workout.tsx
+++ components/Workout.tsx
@@ -7,11 +7,19 @@ export default function Workout({ workout, handleEditWorkout, workoutIndex=-1 }:
         newIntervals[intervalIndex] = newInterval;
         handleEditWorkout({...workout, intervals: newIntervals}, workoutIndex);
     }
+
+    function handleAddInterval(interval, index) {
+        //todo, likely refactor out of Timer for function reuse
+    }
+
+    function handleDeleteInterval(interval, index) {
+        //todo, likely refactor out of Timer for function reuse
+    }
     
     return (
         <div>
             <h2 className="text-xl">{workout.date.toDateString()}</h2>
-            <IntervalsTable intervals={workout.intervals} handleEditInterval={handleEditInterval}/>
+            <IntervalsTable intervals={workout.intervals} handleEditInterval={handleEditInterval} handleAddInterval={handleAddInterval} handleDeleteInterval={handleDeleteInterval}/>
         </div>
     )
 }
